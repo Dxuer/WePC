c5b437256ae600faedd1be1bdcc50ad8792b07b5
e <2368237009@qq.com>
2018-09-05 11:34:25 +0800

    优化

--- WePC.py
+++ WePC.py
@@ -5,7 +5,7 @@ import datetime
 
 
 def getIP():
-    ##获取公网IP
+    # 获取公网IP
     year = str(datetime.datetime.now().year)
     url = "http://" + year + ".ip138.com/ic.asp"
     data = requests.get(url)
@@ -14,38 +14,35 @@ def getIP():
 
 
 def getMemory():
-    ## 获取内存百分比
+    # 获取内存百分比
     m = psutil.virtual_memory().percent
     return str(m)
-    
+
 
 def getCPU(x):
-    ##获取CPU百分比
-    #time.sleep(0.1)
+    # 获取CPU百分比
+    # time.sleep(0.1)
     c = psutil.cpu_percent(interval=x)
     return str(c)
-    
+
 
 def sendServer(desp):
-    ## 发送到微信
+    # 发送到微信
     today = str(datetime.date.today())
     key = "key"
-    url = "https://sc.ftqq.com/"+key+".send"
-    d = {'text': today +'，内存CPU当前百分比',
+    url = "https://sc.ftqq.com/" + key + ".send"
+    d = {'text': "日期：" + today + '，内存CPU当前百分比',
          'desp': desp
          }
-    r = requests.post(url,data=d)
+    r = requests.post(url, data=d)
     return r.text
-    
 
 
 var = 1
 while var == 1:
-    ##无限循环
+    # 无限循环
     ip = getIP()
     x = getCPU(1)
     y = getMemory()
-    send = "###公网IP："+ ip +"，CPU："+ x +"，内存："+ y
+    send = "###公网IP：" + ip + "，CPU：" + x + "，内存：" + y
     sendServer(send)
-    
-    
